e23332d9e5ed603f4c01c65653a644178e284889
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2024-01-04 21:49:28 -0500

    clean up config logic

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import json
 from typing import Optional
 import configparser
 
@@ -12,14 +13,7 @@ def get_config(header: str, key: str, return_type: Optional[type] = str):
         return str(value)
     if return_type is int:
         return int(value)
-    if return_type is bool:
-        if str.lower(value) == 'false':
-            return False
-        return True
-    if return_type is list:
-        value = value.replace('[', '')
-        value = value.replace(']', '')
-        value = value.replace(',', '')
-        return str.split(value)
+    if return_type is list or bool:
+        return json.loads(value)
     raise ValueError('invalid config type')
 
